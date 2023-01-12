26f68eaf3095dfbc3226b0e965ab4b9190523db0
Brayden Chan <braydenmchan@gmail.com>
2023-01-12 09:17:05 -0800

    Add game board

--- src/main/java/com/megabyte6/connect4/controller/GameController.java
+++ src/main/java/com/megabyte6/connect4/controller/GameController.java
@@ -1,6 +1,15 @@
 package com.megabyte6.connect4.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class GameController implements Controller {
+
+    @FXML
+    private GridPane gameBoard;
+
+    private GameController() {
+    }
 
     @Override
     public void initialize() {
