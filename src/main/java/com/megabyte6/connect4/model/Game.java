f201f922f297c95911a8c5b285eefa24e27e73e1
Brayden Chan <braydenmchan@gmail.com>
2023-01-13 17:41:52 -0800

    Add Game object.

--- src/main/java/com/megabyte6/connect4/model/Game.java
+++ src/main/java/com/megabyte6/connect4/model/Game.java
@@ -1,4 +1,23 @@
 package com.megabyte6.connect4.model;
 
+import javafx.scene.paint.Color;
+
 public class Game {
+
+    private Player player1;
+    private Player player2;
+
+    public Game(String player1Name, String player2Name) {
+        player1 = new Player(player1Name, Color.RED);
+        player2 = new Player(player2Name, Color.YELLOW);
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
 }
