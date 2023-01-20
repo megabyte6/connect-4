b6eacc8b04cefa9f6f6a67330f1f92e87e97c4da
Brayden Chan <braydenmchan@gmail.com>
2023-01-19 17:54:53 -0800

    Add Player.

--- src/main/java/com/megabyte6/connect4/model/Game.java
+++ src/main/java/com/megabyte6/connect4/model/Game.java
@@ -4,12 +4,26 @@ import javafx.scene.paint.Color;
 
 public class Game {
 
-    private Player player1;
-    private Player player2;
+    private final Player player1;
+    private final Player player2;
+
+    private Player currentPlayer;
 
     public Game(String player1Name, String player2Name) {
-        player1 = new Player(player1Name, Color.RED);
-        player2 = new Player(player2Name, Color.YELLOW);
+        player1 = new Player(player2Name, Color.YELLOW);
+        player2 = new Player(player1Name, Color.RED);
+
+        currentPlayer = player1;
+    }
+
+    public void swapTurns() {
+        currentPlayer = currentPlayer == player1
+                ? player2
+                : player1;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public Player getPlayer1() {
