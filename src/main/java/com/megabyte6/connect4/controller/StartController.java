30c5b397728460e13076202dd1155ec460607534
Brayden Chan <braydenmchan@gmail.com>
2023-01-30 18:48:07 -0800

    Get player names when the game starts.

--- src/main/java/com/megabyte6/connect4/controller/StartController.java
+++ src/main/java/com/megabyte6/connect4/controller/StartController.java
@@ -1,14 +1,66 @@
 package com.megabyte6.connect4.controller;
 
+import com.megabyte6.connect4.App;
+import com.megabyte6.connect4.model.Player;
 import com.megabyte6.connect4.util.SceneManager;
+import com.megabyte6.connect4.util.tuple.Pair;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
-public class StartController {
+import java.util.function.Consumer;
+
+public class StartController implements Controller {
+
+    @FXML
+    private AnchorPane root;
 
     @FXML
     private void startButtonPressed() {
-        SceneManager.switchScenes("Game", Duration.millis(400));
+        setDisable(true);
+        getPlayer1Name();
     }
 
+    private void getPlayer1Name() {
+        playerNameInputPopup(
+                "Enter the first player's name",
+                (text) -> {
+                    String name = text.isEmpty() || text.isBlank() ? "Player 1" : text;
+                    App.setPlayer1(new Player(name));
+                    getPlayer2Name();
+                },
+                () -> setDisable(false)
+        );
+    }
+
+    private void getPlayer2Name() {
+        playerNameInputPopup(
+                "Enter the second player's name",
+                (text) -> {
+                    String name = text.isEmpty() || text.isBlank() ? "Player 2" : text;
+                    App.setPlayer2(new Player(name));
+                    SceneManager.switchScenes("Game", Duration.millis(400));
+                },
+                () -> setDisable(false)
+        );
+    }
+
+    private void playerNameInputPopup(String promptText, Consumer<String> onOk, Runnable onCancel) {
+        Pair<Node, Controller> loadedData = SceneManager.loadFXMLAndController("TextInput");
+        Node root = loadedData.a();
+        TextInputController controller = (TextInputController) loadedData.b();
+
+        controller.setPromptText(promptText);
+        controller.setOnOK(onOk);
+        controller.setOnCancel(onCancel);
+
+        SceneManager.addScene(root);
+    }
+
+    @Override
+    public void setDisable(boolean disabled) {
+        root.setDisable(disabled);
+        root.setOpacity(disabled ? App.DISABLED_OPACITY : 1);
+    }
 }
