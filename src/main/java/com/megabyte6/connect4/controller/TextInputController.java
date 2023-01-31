6850dd61bfc22ed28fabe97cf948dffc99bb9ea0
Brayden Chan <braydenmchan@gmail.com>
2023-01-30 18:51:25 -0800

    Add TextInput helper layout and controller.

---
+++ src/main/java/com/megabyte6/connect4/controller/TextInputController.java
@@ -0,0 +1,68 @@
+package com.megabyte6.connect4.controller;
+
+import com.megabyte6.connect4.App;
+import com.megabyte6.connect4.util.SceneManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.function.Consumer;
+
+public class TextInputController implements Controller {
+
+    private Consumer<String> onOK = (text) -> {
+    };
+    private Runnable onCancel = () -> {
+    };
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Label promptLabel;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    public void initialize() {
+        textField.setOnAction((event) -> {
+            SceneManager.removeTopScene();
+            onOK.accept(textField.getText());
+        });
+
+        textField.requestFocus();
+    }
+
+    public void setPromptText(String promptText) {
+        promptLabel.setText(promptText);
+    }
+
+    public void setOnOK(Consumer<String> runAfter) {
+        onOK = runAfter;
+    }
+
+    public void setOnCancel(Runnable runAfter) {
+        onCancel = runAfter;
+    }
+
+    @FXML
+    private void handleOkButton() {
+        SceneManager.removeTopScene();
+        onOK.accept(textField.getText());
+    }
+
+    @FXML
+    private void handleCancelButton() {
+        SceneManager.removeTopScene();
+        onCancel.run();
+    }
+
+    @Override
+    public void setDisable(boolean disabled) {
+        root.setDisable(disabled);
+        root.setOpacity(disabled ? App.DISABLED_OPACITY : 1);
+    }
+
+}
