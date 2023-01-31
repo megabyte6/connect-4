dce8500000c534765e47001ab210a3e72da3c393
Brayden Chan <braydenmchan@gmail.com>
2023-01-30 18:33:38 -0800

    Give player management to the main App class.

--- src/main/java/com/megabyte6/connect4/App.java
+++ src/main/java/com/megabyte6/connect4/App.java
@@ -1,7 +1,7 @@
 package com.megabyte6.connect4;
 
+import com.megabyte6.connect4.model.Player;
 import com.megabyte6.connect4.util.SceneManager;
-
 import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.scene.image.Image;
@@ -12,18 +12,12 @@ public class App extends Application {
 
     public static final int WIN_REQUIREMENT = 4;
     public static final Color BACKGROUND_COLOR = Color.web("#2d2d2d");
+    public static final double DISABLED_OPACITY = 0.8;
+    public static Player player1 = Player.NONE;
+    public static Player player2 = Player.NONE;
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        SceneManager.init(primaryStage, "Start", BACKGROUND_COLOR);
-
-        primaryStage.getIcons().add(new Image("icon.png"));
-        primaryStage.setTitle("Connect 4");
-        primaryStage.show();
     }
 
     public static void delay(long millis, Runnable runAfter) {
@@ -48,6 +42,31 @@ public class App extends Application {
         sleep.setOnSucceeded(event -> runAfter.run());
 
         new Thread(sleep);
+    }
+
+    public static Player getPlayer1() {
+        return App.player1;
+    }
+
+    public static void setPlayer1(Player player) {
+        App.player1 = player;
+    }
+
+    public static Player getPlayer2() {
+        return App.player2;
+    }
+
+    public static void setPlayer2(Player player) {
+        App.player2 = player;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        SceneManager.init(primaryStage, "Start", BACKGROUND_COLOR);
+
+        primaryStage.getIcons().add(new Image("icon.png"));
+        primaryStage.setTitle("Connect 4");
+        primaryStage.show();
     }
 
 }
