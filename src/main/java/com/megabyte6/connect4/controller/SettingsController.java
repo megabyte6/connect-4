12ade8f43c536e9586263bd4b44709469d8e316b
Brayden Chan <braydenmchan@gmail.com>
2023-02-04 22:24:25 -0800

    Make settings screen responsive.

--- src/main/java/com/megabyte6/connect4/controller/SettingsController.java
+++ src/main/java/com/megabyte6/connect4/controller/SettingsController.java
@@ -3,9 +3,10 @@ package com.megabyte6.connect4.controller;
 import com.megabyte6.connect4.App;
 import com.megabyte6.connect4.util.SceneManager;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import static com.megabyte6.connect4.util.Range.range;
 
@@ -15,11 +16,14 @@ public class SettingsController implements Controller {
     };
 
     @FXML
+    private AnchorPane root;
+
+    @FXML
     private TextField columnCount;
     @FXML
     private TextField rowCount;
     @FXML
-    private ChoiceBox<Integer> goalLength;
+    private ComboBox<Integer> winningLength;
 
     @FXML
     private ColorPicker player1Color;
@@ -31,51 +35,83 @@ public class SettingsController implements Controller {
         columnCount.setText(App.getColumnCount() + "");
         rowCount.setText(App.getRowCount() + "");
 
-        for (int i : range(1, App.getWinRequirement()))
-            goalLength.getItems().add(i);
+        updateGoalLengthOptions();
 
         player1Color.setValue(App.getPlayer1().getColor());
         player2Color.setValue(App.getPlayer2().getColor());
 
-        columnCount.setOnAction(event -> inputValid());
-        rowCount.setOnAction(event -> inputValid());
-        goalLength.setOnAction(event -> inputValid());
-        player1Color.setOnAction(event -> inputValid());
-        player2Color.setOnAction(event -> inputValid());
+        columnCount.setOnKeyTyped(event -> {
+            if (inputValid()) {
+                updateGoalLengthOptions();
+                updateAppSettings();
+            }
+        });
+        rowCount.setOnKeyTyped(event -> {
+            if (inputValid()) {
+                updateGoalLengthOptions();
+                updateAppSettings();
+            }
+        });
+        winningLength.setOnAction(event -> {
+            if (inputValid())
+                updateAppSettings();
+        });
+        player1Color.setOnAction(event -> {
+            if (inputValid())
+                updateAppSettings();
+        });
+        player2Color.setOnAction(event -> {
+            if (inputValid())
+                updateAppSettings();
+        });
     }
 
     private boolean inputValid() {
         int selectedColumnCount, selectedRowCount;
+
+        columnCount.setStyle("-fx-text-fill: white;"
+                + "-fx-background-color: #3d3d3d;");
         try {
             selectedColumnCount = Integer.parseInt(columnCount.getText());
-            columnCount.setStyle("-fx-text-fill: white;"
-                    + "-fx-background-color: #3d3d3d;");
         } catch (NumberFormatException e) {
-            columnCount.setStyle("-fx-text-fill: white;"
-                    + "-fx-background-color: #3d3d3d;"
-                    + "-fx-border-color: red;");
+            columnCount.setStyle(columnCount.getStyle() + "-fx-border-color: red;");
             return false;
         }
+
+        rowCount.setStyle("-fx-text-fill: white;"
+                + "-fx-background-color: #3d3d3d;");
         try {
             selectedRowCount = Integer.parseInt(rowCount.getText());
-            rowCount.setStyle("-fx-text-fill: white;"
-                    + "-fx-background-color: #3d3d3d;");
         } catch (NumberFormatException e) {
-            rowCount.setStyle("-fx-text-fill: white;"
-                    + "-fx-background-color: #3d3d3d;"
-                    + "-fx-border-color: red;");
+            rowCount.setStyle(rowCount.getStyle() + "-fx-border-color: red;");
             return false;
         }
 
-        int selectedGoalLength = goalLength.getSelectionModel().getSelectedItem();
+        winningLength.setStyle("-fx-background-color: #3d3d3d;");
+        int selectedGoalLength = winningLength.getSelectionModel().getSelectedIndex() + 1;
         if (selectedGoalLength > Math.min(selectedColumnCount, selectedRowCount)) {
-            goalLength.setStyle("-fx-background-color: #3d3d3d;"
-                    + "-fx-border-color: red;");
+            winningLength.setStyle(winningLength.getStyle() + "-fx-border-color: red;");
             return false;
         }
-        goalLength.setStyle("-fx-background-color: #3d3d3d;");
 
         return true;
+    }
+
+    private void updateGoalLengthOptions() {
+        winningLength.getItems().clear();
+        int maxGoalLengthOption = Math.min(App.getColumnCount(), App.getRowCount());
+        for (int i : range(maxGoalLengthOption))
+            winningLength.getItems().add(i + 1);
+
+        winningLength.setValue(App.getWinRequirement());
+    }
+
+    private void updateAppSettings() {
+        App.setColumnCount(Integer.parseInt(columnCount.getText()));
+        App.setRowCount(Integer.parseInt(rowCount.getText()));
+        App.setWinRequirement(winningLength.getSelectionModel().getSelectedIndex() + 1);
+        App.getPlayer1().setColor(player1Color.getValue());
+        App.getPlayer2().setColor(player2Color.getValue());
     }
 
     public void setOnClosed(Runnable run) {
@@ -93,7 +129,7 @@ public class SettingsController implements Controller {
 
         App.setColumnCount(Integer.parseInt(columnCount.getText()));
         App.setRowCount(Integer.parseInt(rowCount.getText()));
-        App.setWinRequirement(goalLength.getSelectionModel().getSelectedItem());
+        App.setWinRequirement(winningLength.getSelectionModel().getSelectedItem());
         App.getPlayer1().setColor(player1Color.getValue());
         App.getPlayer2().setColor(player2Color.getValue());
 
@@ -102,5 +138,7 @@ public class SettingsController implements Controller {
 
     @Override
     public void setDisable(boolean disabled) {
+        root.setDisable(disabled);
+        root.setOpacity(disabled ? App.DISABLED_OPACITY : 1);
     }
 }
