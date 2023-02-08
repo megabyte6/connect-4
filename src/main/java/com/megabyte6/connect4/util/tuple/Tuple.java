5103453d6f0b559c8f3f75527dd3b14472067aae
Brayden Chan <braydenmchan@gmail.com>
2023-02-08 11:45:23 -0800

    Remove duplicate code.

--- src/main/java/com/megabyte6/connect4/util/tuple/Tuple.java
+++ src/main/java/com/megabyte6/connect4/util/tuple/Tuple.java
@@ -3,19 +3,19 @@ package com.megabyte6.connect4.util.tuple;
 public class Tuple {
 
     public static <A, B> Pair<A, B> of(A a, B b) {
-        return new Pair<A, B>(a, b);
+        return new Pair<>(a, b);
     }
 
     public static <A, B, C> Triplet<A, B, C> of(A a, B b, C c) {
-        return new Triplet<A, B, C>(a, b, c);
+        return new Triplet<>(a, b, c);
     }
 
     public static <A, B, C, D> Quartet<A, B, C, D> of(A a, B b, C c, D d) {
-        return new Quartet<A, B, C, D>(a, b, c, d);
+        return new Quartet<>(a, b, c, d);
     }
 
     public static <A, B, C, D, E> Quintet<A, B, C, D, E> of(A a, B b, C c, D d, E e) {
-        return new Quintet<A, B, C, D, E>(a, b, c, d, e);
+        return new Quintet<>(a, b, c, d, e);
     }
 
 }
