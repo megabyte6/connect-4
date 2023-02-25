e8d77cf4ffc9e8f7065013f4ae57297a13ba456b
Brayden Chan <braydenmchan@gmail.com>
2023-02-25 00:01:14 -0800

    Add timer settings.

--- src/main/java/com/megabyte6/connect4/model/Settings.java
+++ src/main/java/com/megabyte6/connect4/model/Settings.java
@@ -1,5 +1,6 @@
 package com.megabyte6.connect4.model;
 
+import static java.util.Objects.requireNonNullElse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import com.electronwill.nightconfig.core.file.FileConfig;
@@ -12,22 +13,31 @@ import lombok.NonNull;
 @Data
 public class Settings {
 
-    public static final Settings DEFAULT = new Settings(7, 6, 4, Color.YELLOW, Color.RED);
+    public static final Settings DEFAULT = new Settings(7, 6, 4, false, 0, Color.YELLOW, Color.RED);
 
     private int columnCount;
     private int rowCount;
 
     private int winRequirement;
 
+    private boolean timerEnabled;
+    private int timerLength;
+
     @NonNull
     private Color player1Color;
     @NonNull
     private Color player2Color;
 
-    public Settings(int columnCount, int rowCount, int winRequirement, Color player1Color, Color player2Color) {
+    public Settings(int columnCount, int rowCount, int winRequirement, boolean timerEnabled, int timerLength,
+            Color player1Color, Color player2Color) {
         this.columnCount = columnCount;
         this.rowCount = rowCount;
+
         this.winRequirement = winRequirement;
+
+        this.timerEnabled = timerEnabled;
+        this.timerLength = timerLength;
+
         this.player1Color = player1Color;
         this.player2Color = player2Color;
 
@@ -55,16 +65,39 @@ public class Settings {
         config.set("columnCount", columnCount);
         config.set("rowCount", rowCount);
         config.set("winRequirement", winRequirement);
+        config.set("timerEnabled", timerEnabled);
+        config.set("timerLength", timerLength);
         config.set("player1Color", player1Color.toString());
         config.set("player2Color", player2Color.toString());
 
         config.save();
     }
 
-    @Override
-    public String toString() {
-        return "Settings [columnCount=" + columnCount + ", rowCount=" + rowCount + ", winRequirement=" + winRequirement
-                + ", player1Color=" + player1Color + ", player2Color=" + player2Color + "]";
+    public static Settings load(Path path) {
+        if (Files.notExists(path) || Files.isDirectory(path))
+            return Settings.DEFAULT;
+
+        @Cleanup
+        final FileConfig config = FileConfig.of(path);
+        config.load();
+
+        final Settings df = Settings.DEFAULT;
+        final int columnCount = requireNonNullElse(config.get("columnCount"), df.getColumnCount());
+        final int rowCount = requireNonNullElse(config.get("rowCount"), df.getRowCount());
+        final int winRequirement = requireNonNullElse(config.get("winRequirement"), df.getWinRequirement());
+        final boolean timerEnabled = requireNonNullElse(config.get("timerEnabled"), df.isTimerEnabled());
+        final int timerLength = requireNonNullElse(config.get("timerLength"), df.getTimerLength());
+        final String player1Color = requireNonNullElse(config.get("player1Color"), df.getPlayer1Color().toString());
+        final String player2Color = requireNonNullElse(config.get("player2Color"), df.getPlayer2Color().toString());
+
+        return new Settings(
+                columnCount,
+                rowCount,
+                winRequirement,
+                timerEnabled,
+                timerLength,
+                Color.valueOf(player1Color),
+                Color.valueOf(player2Color));
     }
 
 }
